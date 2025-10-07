dfefeac6e692869b7938dd34d6b452e321caf73a
yRomulo <romulo.marroso>
2025-10-07 18:03:16 -0300

    fix: resolution

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia n1 = new Nokia("123456789", "N1", "123", 16);
+n1.Ligar();
+n1.InstalarAplicativo("WhatsApp");
+
+Iphone i1 = new Iphone("987654321", "I1", "456", 32);
+i1.Ligar();
+i1.InstalarAplicativo("Telegram");
